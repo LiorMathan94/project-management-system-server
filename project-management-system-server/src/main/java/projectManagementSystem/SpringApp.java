4345b879cdfbfd9e51e8c40bafbbdbb48322d051
Lior Mathan <115080216+LiorMathan94@users.noreply.github.com>
2022-12-15 15:28:12 +0200

    Update SpringApp.java

--- project-management-system-server/src/main/java/projectManagementSystem/SpringApp.java
+++ project-management-system-server/src/main/java/projectManagementSystem/SpringApp.java
@@ -1,9 +1,7 @@
 package projectManagementSystem;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import projectManagementSystem.service.FilterCriteriaService;
 
 @SpringBootApplication
 public class SpringApp {
