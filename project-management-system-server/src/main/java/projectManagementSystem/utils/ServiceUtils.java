d098c4daef9e27ce23beb24c00b738095e5440ba
D-Y-So <68039134+D-Y-So@users.noreply.github.com>
2022-12-15 16:00:17 +0200

    save changes before pull

--- project-management-system-server/src/main/java/projectManagementSystem/utils/ServiceUtils.java
+++ project-management-system-server/src/main/java/projectManagementSystem/utils/ServiceUtils.java
@@ -4,9 +4,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class ServiceUtils {
 
     /**
-     * Encrypts a given string using
+     * Encrypts a given password String using: spring-security-crypto bcrypt.BCryptPasswordEncoder
+     *
      * @param password - String, the password that needs to be encrypted
-     * @return the encrypted String.
+     * @return the encrypted password String.
      */
     public static String encryptPassword(String password) {
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
