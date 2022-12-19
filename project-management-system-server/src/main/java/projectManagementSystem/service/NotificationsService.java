95e734716a99b9c233db8c603ebb11762b6bc894
D-Y-So <68039134+D-Y-So@users.noreply.github.com>
2022-12-19 14:37:10 +0200

    save changes before merge

---
+++ project-management-system-server/src/main/java/projectManagementSystem/service/NotificationsService.java
@@ -0,0 +1,2 @@
+package projectManagementSystem.service;public class NotofocationService {
+}
---
+++ project-management-system-server/src/main/java/projectManagementSystem/utils/EmailSender.java
@@ -0,0 +1,36 @@
+package projectManagementSystem.utils;
+
+;
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.googleapis.json.GoogleJsonError;
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.gmail.Gmail;
+import com.google.api.services.gmail.GmailScopes;
+import com.google.api.services.gmail.model.Message;
+import org.apache.commons.codec.binary.Base64;
+
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import java.io.*;
+import java.nio.file.Paths;
+import java.security.GeneralSecurityException;
+
+import java.util.Objects;
+import java.util.Properties;
+import java.util.Set;
+
+public class EmailUtils {
+
+
+}
