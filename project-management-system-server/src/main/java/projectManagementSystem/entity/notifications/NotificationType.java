7984e2c65b12e5696cf13fb6605a0e7e5a36b4b7
shilatorkaby <shillat14@gmail.com>
2022-12-21 12:08:13 +0200

    add notification setting

---
+++ project-management-system-server/src/main/java/projectManagementSystem/entity/notifications/NotificationType.java
@@ -0,0 +1,10 @@
+package projectManagementSystem.entity.notifications;
+
+public enum NotificationType {
+    ASSIGNED_ITEM,
+    ITEM_STATUS_CHANGED,
+    COMMENT_ADDED,
+    ITEM_DELETED,
+    ITEM_CONTENT_CHANGED,
+    USER_JOINED_TO_BOARD
+}
