c967c53b9ff1ddb49b67afe1cb7f314e7ef21df0
D-Y-So <68039134+D-Y-So@users.noreply.github.com>
2022-12-21 15:12:34 +0200

    added javadoc to EmailUtil

--- project-management-system-server/src/main/java/projectManagementSystem/utils/EmailUtil.java
+++ project-management-system-server/src/main/java/projectManagementSystem/utils/EmailUtil.java
@@ -5,6 +5,12 @@ import org.springframework.mail.SimpleMailMessage;
 public class EmailUtil {
     private static final String FROM = "startgooglproject@gmail.com";
 
+    /**
+     * @param recipient - String, email address of the recipient.
+     * @param subject   - String, subject of the email.
+     * @param body      - String, content of the email.
+     * @return SimpleMailMessage object - includes the data: from email address, to email address, email subject, and email body text.
+     */
     public static SimpleMailMessage prepareMessage(String recipient, String subject, String body) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom(FROM);
