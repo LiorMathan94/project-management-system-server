f571f5778448fcdd59092b3e5f4dbe8d04589255
Lior Mathan <Lior.mathan@gmail.com>
2022-12-26 16:11:57 +0200

    Added: documentation.

--- project-management-system-server/src/main/java/projectManagementSystem/entity/notifications/NotificationPreference.java
+++ project-management-system-server/src/main/java/projectManagementSystem/entity/notifications/NotificationPreference.java
@@ -30,36 +30,65 @@ public class NotificationPreference {
     public NotificationPreference() {
     }
 
+    /**
+     * Constructor for notification preferences.
+     * @param user
+     */
     public NotificationPreference(User user) {
         this.boardActions = new ArrayList<>();
         this.notificationViaList = new ArrayList<>();
         this.user = user;
     }
 
+    /**
+     * @return notification's ID
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     * @return notification's boards action list, to notify for
+     */
     public List<BoardAction> getNotifyOnEvents() {
         return boardActions;
     }
 
+    /**
+     * @return notification's notify via list
+     */
     public List<NotificationVia> getNotificationViaList() {
         return notificationViaList;
     }
 
+    /**
+     * Sets notification's board actions.
+     * @param boardActions
+     */
     public void setBoardActions(List<BoardAction> boardActions) {
         this.boardActions = boardActions;
     }
 
+    /**
+     * Sets notification's via list.
+     * @param notificationViaList
+     */
     public void setNotificationViaList(List<NotificationVia> notificationViaList) {
         this.notificationViaList = notificationViaList;
     }
 
+    /**
+     * Adds notification event (board action) to notify for.
+     * @param boardActions
+     */
     public void addNotificationEvent(BoardAction boardActions) {
         this.boardActions.add(boardActions);
     }
 
+    /**
+     * Adds notification method.
+     * @param notificationVia
+     */
     public void addNotificationVia(NotificationVia notificationVia) {
         this.notificationViaList.add(notificationVia);
     }
