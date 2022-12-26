dad5eac504d9493c3c5ff69271aefe0d68b03f83
D-Y-So <68039134+D-Y-So@users.noreply.github.com>
2022-12-26 09:25:01 +0200

    added user role to AuthorizedUserDTO

--- project-management-system-server/src/main/java/projectManagementSystem/entity/DTO/AuthorizedUserDTO.java
+++ project-management-system-server/src/main/java/projectManagementSystem/entity/DTO/AuthorizedUserDTO.java
@@ -1,14 +1,17 @@
 package projectManagementSystem.entity.DTO;
 
 import projectManagementSystem.entity.AuthorizedUser;
+import projectManagementSystem.entity.Role;
 
 public class AuthorizedUserDTO {
     private long id;
     private String email;
+    private Role role;
 
     public AuthorizedUserDTO(AuthorizedUser authorizedUser) {
         this.id = authorizedUser.getUser().getId();
         this.email = authorizedUser.getUser().getEmail();
+        this.role = authorizedUser.getRole();
     }
 
     public long getId() {
@@ -19,4 +22,7 @@ public class AuthorizedUserDTO {
         return email;
     }
 
+    public Role getRole() {
+        return role;
+    }
 }
